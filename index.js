edd3cbaae22e3ebbb6118249c86785f6cdee1a91
Ibrahim Hernández Jorge <ibrahimhernandezjorge@gmail.com>
2017-03-30 21:47:50 +0100

    Moviendo fichero index.js a lugar adecuado

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var path    = require('path');
 process.env.PWD = process.cwd();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(path.join(process.env.PWD, '/public')));
+app.use(express.static(path.join(process.env.PWD, "/public")));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
